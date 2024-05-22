00f6cf9f4221865e1a28d51132677380dc967c19
Captain Yoshi <captain.yoshisaur@gmail.com>
2024-05-22 13:50:37 -0400

    [refac] bt_demo
    Minimalist demo for viewing an MTC plan with RViz.

---
+++ examples/bt_demo.cpp
@@ -0,0 +1,78 @@
+#include <ros/ros.h>
+
+#include <behaviortree_mtc/initialize_mtc_task.h>
+#include <behaviortree_mtc/create_mtc_current_state.h>
+#include <behaviortree_mtc/move_mtc_stage_to_container.h>
+#include <behaviortree_mtc/plan_mtc_task.h>
+
+#include "behaviortree_cpp/bt_factory.h"
+#include "behaviortree_cpp/loggers/bt_file_logger_v2.h"
+#include "behaviortree_cpp/loggers/groot2_publisher.h"
+
+using namespace BT;
+
+// clang-format off
+static const char* xml_text = R"(
+
+ <root BTCPP_format="4" >
+
+     <BehaviorTree ID="MainTree">
+        <Sequence name="root">
+            <InitializeMTCTask        task="{mtc_task}" container="{container}" />
+            <CreateMTCCurrentState    stage="{stage}" />
+            <MoveMTCStageToContainer  container="{container}" stage="{stage}" />
+            <PlanMTCTask              task="{mtc_task}" max_solutions="5" />
+        </Sequence>
+     </BehaviorTree>
+
+ </root>
+ )";
+// clang-format on
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "test_behavior_tree");
+
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  BehaviorTreeFactory factory;
+
+  factory.registerNodeType<InitializeMTCTask>("InitializeMTCTask");
+  factory.registerNodeType<CreateMTCCurrentState>("CreateMTCCurrentState");
+  factory.registerNodeType<MoveMTCStageToContainer>("MoveMTCStageToContainer");
+  factory.registerNodeType<PlanMTCTask>("PlanMTCTask");
+
+  auto tree = factory.createTreeFromText(xml_text);
+
+  // Connect the Groot2Publisher. This will allow Groot2 to
+  // get the tree and poll status updates.
+  const unsigned port = 1667;
+  BT::Groot2Publisher publisher(tree, port);
+
+  // Add two more loggers, to save the transitions into a file.
+  // Both formats are compatible with Groot2
+
+  // Logging with lightweight serialization
+  BT::FileLogger2 logger2(tree, "t12_logger2.btlog");
+
+  // Gives the user time to connect to Groot2
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+  NodeStatus status = NodeStatus::IDLE;
+
+  while(ros::ok() && (status == NodeStatus::IDLE || status == NodeStatus::RUNNING))
+  {
+    // ros::spinOnce();
+    std::cout << "Start" << std::endl;
+    status = tree.tickExactlyOnce();
+    std::cout << status << std::endl;
+
+    ros::Duration sleep_time(0.01);
+    sleep_time.sleep();
+  }
+
+  ros::waitForShutdown();
+
+  return 0;
+}
