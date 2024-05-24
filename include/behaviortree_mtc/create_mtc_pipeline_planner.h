5cd44af9fc7b29e97c0dfd933b721f26504125b6
Captain Yoshi <captain.yoshisaur@gmail.com>
2024-05-24 08:38:08 -0400

    [fix] goal joint tolerance param is optional

--- include/behaviortree_mtc/create_mtc_pipeline_planner.h
+++ include/behaviortree_mtc/create_mtc_pipeline_planner.h
@@ -1,22 +1,5 @@
 #pragma once
 
-<<<<<<< HEAD
-#include <moveit/task_constructor/solvers/pipeline_planner.h>
-#include <behaviortree_cpp/bt_factory.h>
-
-namespace MTC = moveit::task_constructor;
-namespace bt_mtc{
-class CreatePipelinePlanner : public BT::SyncActionNode
-{
-public:
-  CreatePipelinePlanner(const std::string& name, const BT::NodeConfig& config);
-  void setPipelineProperties(std::string property_name,std::string type, std::shared_ptr<MTC::solvers::PipelinePlanner> pipelinePlanner);
-  BT::NodeStatus tick() override;
-  static BT::PortsList providedPorts();
-};
-}
-
-=======
 #include <behaviortree_cpp/bt_factory.h>
 
 namespace bt_mtc
@@ -30,4 +13,3 @@ public:
   static BT::PortsList providedPorts();
 };
 }  // namespace bt_mtc
->>>>>>> [impl] pipeline planner solver
