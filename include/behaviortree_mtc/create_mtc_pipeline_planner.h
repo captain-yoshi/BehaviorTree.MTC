0199b433c569da5af188c5d8813f207c2ae95886
bob <berg0401@usherbrooke.ca>
2024-05-27 10:26:03 -0400

    create_mtc_task completed

--- include/behaviortree_mtc/create_mtc_pipeline_planner.h
+++ include/behaviortree_mtc/create_mtc_pipeline_planner.h
@@ -1,17 +1,14 @@
 #pragma once
 
-#include <moveit/task_constructor/solvers/pipeline_planner.h>
 #include <behaviortree_cpp/bt_factory.h>
 
-namespace MTC = moveit::task_constructor;
-namespace bt_mtc{
+namespace bt_mtc
+{
 class CreatePipelinePlanner : public BT::SyncActionNode
 {
 public:
   CreatePipelinePlanner(const std::string& name, const BT::NodeConfig& config);
-  void setPipelineProperties(std::string property_name,std::string type, std::shared_ptr<MTC::solvers::PipelinePlanner> pipelinePlanner);
   BT::NodeStatus tick() override;
   static BT::PortsList providedPorts();
 };
-}
-
+}  // namespace bt_mtc
