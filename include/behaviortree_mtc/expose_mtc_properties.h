455d2a651993b48465fc11f775847fa6d55a8ffc
Captain Yoshi <captain.yoshisaur@gmail.com>
2024-06-11 10:39:14 -0400

    [impl] BT node to expose mtc properties

---
+++ include/behaviortree_mtc/expose_mtc_properties.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <behaviortree_cpp/bt_factory.h>
+
+#include <moveit/task_constructor/task.h>
+
+namespace bt_mtc
+{
+template <typename FROM, typename TO>
+class ExposeMTCProperties : public BT::SyncActionNode
+{
+public:
+  ExposeMTCProperties(const std::string& name, const BT::NodeConfig& config)
+    : SyncActionNode(name, config)
+  {}
+
+  BT::NodeStatus tick() override
+  {
+    std::shared_ptr<FROM> from{ nullptr };
+    std::shared_ptr<TO> to{ nullptr };
+    std::shared_ptr<std::set<std::string>> property_names{ nullptr };
+
+    if(!getInput("property_names", property_names) ||
+       !getInput("from", from) ||
+       !getInput("to", to))
+      return BT::NodeStatus::FAILURE;
+
+    from->properties().exposeTo(to->properties(), *property_names);
+
+    return BT::NodeStatus::SUCCESS;
+  }
+
+  static BT::PortsList providedPorts()
+  {
+    return {
+      BT::InputPort<std::shared_ptr<std::set<std::string>>>("property_names"),
+
+      BT::BidirectionalPort<std::shared_ptr<FROM>>("from"),
+      BT::BidirectionalPort<std::shared_ptr<TO>>("to"),
+    };
+  }
+};
+
+}  // namespace bt_mtc
