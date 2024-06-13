8e58b3a201d66e25aa4f0eb668e37de75142a53e
Captain Yoshi <captain.yoshisaur@gmail.com>
2024-06-13 09:11:53 -0400

    [refac] allow/forbid collisions
    Use inheritance to reduce boilerplate code. For the JMG case, supply the
    mtc task and the joint model group name to extract the links.

--- src/create_mtc_modify_planning_scene.cpp
+++ src/create_mtc_modify_planning_scene.cpp
@@ -2,6 +2,8 @@
 #include <behaviortree_mtc/shared_to_unique.h>
 
 #include <moveit/task_constructor/stages/modify_planning_scene.h>
+#include <moveit/task_constructor/task.h>
+#include <moveit/robot_model/robot_model.h>
 
 using namespace BT;
 using namespace bt_mtc;
@@ -47,4 +49,121 @@ BT::PortsList AttachDetachObjects::providedPorts()
     BT::InputPort<std::vector<std::string>>("link_name", "attach or detach a list of objects to the given link"),
     BT::OutputPort<MTC::StagePtr>("stage", "ModifyPlanningScene stage"),
   };
+}
+
+AllowForbidCollisions::AllowForbidCollisions(const std::string& name, const BT::NodeConfig& config, bool allow)
+  : SyncActionNode(name, config), _allow(allow)
+{}
+
+BT::NodeStatus AllowForbidCollisions::tick(const std::vector<std::string>& first, const std::vector<std::string>& second)
+{
+  std::string stage_name;
+
+  if(!getInput("stage_name", stage_name))
+    return NodeStatus::FAILURE;
+
+  // build stage
+  std::shared_ptr<MTC::stages::ModifyPlanningScene> stage{ nullptr };
+  stage = {
+    new MTC::stages::ModifyPlanningScene(stage_name),
+    dirty::fake_deleter{}
+  };
+
+  stage->allowCollisions(first, second, _allow);
+
+  // upcast to base class
+  MTC::StagePtr base_stage = stage;
+
+  setOutput("stage", base_stage);
+
+  return NodeStatus::SUCCESS;
+}
+
+BT::PortsList AllowForbidCollisions::providedPorts()
+{
+  return {
+    BT::InputPort<std::string>("stage_name"),
+    BT::OutputPort<MTC::StagePtr>("stage", "ModifyPlanningScene stage"),
+  };
+}
+
+AllowForbidCollisionPairs::AllowForbidCollisionPairs(const std::string& name, const BT::NodeConfig& config, bool allow)
+  : AllowForbidCollisions(name, config, allow)
+{}
+
+BT::NodeStatus AllowForbidCollisionPairs::tick()
+{
+  std::shared_ptr<std::vector<std::string>> first_names;
+  std::shared_ptr<std::vector<std::string>> second_names;
+
+  if(!getInput("first", first_names) ||
+     !getInput("second", second_names))
+    return NodeStatus::FAILURE;
+
+  return AllowForbidCollisions::tick(*first_names, *second_names);
+}
+
+BT::PortsList AllowForbidCollisionPairs::providedPorts()
+{
+  auto port_lists = AllowForbidCollisions::providedPorts();
+  port_lists.emplace(BT::InputPort<std::shared_ptr<std::vector<std::string>>>("first", "first object of collision pair"));
+  port_lists.emplace(BT::InputPort<std::shared_ptr<std::vector<std::string>>>("second", "second object of collision pair"));
+
+  return port_lists;
+}
+
+AllowForbidAllCollisions::AllowForbidAllCollisions(const std::string& name, const BT::NodeConfig& config, bool allow)
+  : AllowForbidCollisions(name, config, allow)
+{}
+
+BT::NodeStatus AllowForbidAllCollisions::tick()
+{
+  std::shared_ptr<std::vector<std::string>> objects;
+
+  if(!getInput("objects", objects))
+    return NodeStatus::FAILURE;
+
+  return AllowForbidCollisions::tick(*objects, std::vector<std::string>());
+}
+
+BT::PortsList AllowForbidAllCollisions::providedPorts()
+{
+  auto port_lists = AllowForbidCollisions::providedPorts();
+  port_lists.emplace(BT::InputPort<std::shared_ptr<std::vector<std::string>>>("objects", "allow/forbid all collisions for given object/s"));
+
+  return port_lists;
+}
+
+AllowForbidCollisionsJointModelGroup::AllowForbidCollisionsJointModelGroup(const std::string& name, const BT::NodeConfig& config, bool allow)
+  : AllowForbidCollisions(name, config, allow)
+{}
+
+BT::NodeStatus AllowForbidCollisionsJointModelGroup::tick()
+{
+  std::shared_ptr<std::vector<std::string>> objects;
+  moveit::task_constructor::TaskPtr task;
+  std::string jmg_name;
+
+  if(!getInput("objects", objects) ||
+     !getInput("jmg_name", jmg_name) ||
+     !getInput("task", task))
+    return NodeStatus::FAILURE;
+
+  if(auto jmg = task->getRobotModel()->getJointModelGroup(jmg_name))
+  {
+    const auto& links = jmg->getLinkModelNamesWithCollisionGeometry();
+
+    return AllowForbidCollisions::tick(*objects, links);
+  }
+  return NodeStatus::FAILURE;
+}
+
+BT::PortsList AllowForbidCollisionsJointModelGroup::providedPorts()
+{
+  auto port_lists = AllowForbidCollisions::providedPorts();
+  port_lists.emplace(BT::InputPort<std::shared_ptr<std::vector<std::string>>>("objects", "allow/forbid collisions with joint model group links"));
+  port_lists.emplace(BT::InputPort<std::string>("jmg_name", "joint model group name"));
+  port_lists.emplace(BT::InputPort<moveit::task_constructor::TaskPtr>("task", "MTC task"));
+
+  return port_lists;
 }
