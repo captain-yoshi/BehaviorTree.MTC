2f957dcd57e3f0f88eba217c0129561004ad8e54
Captain Yoshi <captain.yoshisaur@gmail.com>
2024-11-05 17:14:23 -0500

    Make MTC planning preemptable (#26)
    We must stop the planning thread fast when the halt() method is invoked.
    https://www.behaviortree.dev/docs/guides/asynchronous_nodes#the-problem-with-multi-threading

---
+++ tests/models.cpp
@@ -0,0 +1,25 @@
+#include "models.h"
+#include <moveit/utils/robot_model_test_utils.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+
+using namespace moveit::core;
+
+RobotModelPtr getModel()
+{
+  // suppress RobotModel errors and warnings
+  ros::console::set_logger_level(ROSCONSOLE_ROOT_LOGGER_NAME ".moveit_core.robot_model", ros::console::levels::Fatal);
+
+  // create dummy robot model
+  moveit::core::RobotModelBuilder builder("robot", "base");
+  builder.addChain("base->link1->link2->tip", "continuous");
+  builder.addGroupChain("base", "link2", "group");
+  builder.addGroupChain("link2", "tip", "eef_group");
+  builder.addEndEffector("eef", "link2", "group", "eef_group");
+  return builder.build();
+}
+
+moveit::core::RobotModelPtr loadModel()
+{
+  static robot_model_loader::RobotModelLoader loader;
+  return loader.getModel();
+}
