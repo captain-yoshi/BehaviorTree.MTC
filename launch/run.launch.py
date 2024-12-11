6b96246501af6b46e1f66d399b9b641a9440f2b8
DaniGarciaLopez <d.garcialopez@hotmail.com>
2024-12-11 23:17:15 +0100

    Rolling port

---
+++ launch/run.launch.py
@@ -0,0 +1,31 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+def generate_launch_description():
+    moveit_config = (
+        MoveItConfigsBuilder("moveit_resources_panda")
+        .robot_description(file_path="config/panda.urdf.xacro")
+        .to_moveit_configs()
+    )
+
+    node = Node(
+        package="behaviortree_mtc",
+        executable="bt_demo",
+        output="screen",
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            moveit_config.joint_limits,
+            moveit_config.planning_pipelines,
+            os.path.join(get_package_share_directory("moveit_task_constructor_demo"), "config", "panda_config.yaml"),
+        ],
+    )
+
+    return LaunchDescription([node])
